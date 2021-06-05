5b0c7aa93cfafa916f50e6a80c39afa6ce371a26
puni0000 <58594202+puni0000@users.noreply.github.com>
2021-06-05 12:01:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-git clone https://github.com/xiaorouji/openwrt-passwall
+git clone https://github.com/kenzok8/small.git
